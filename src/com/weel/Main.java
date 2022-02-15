c688fa3120e892153fddef7002901296378777f0
Nathan Weel <nathanweel09@gmail.com>
2022-02-14 18:48:35 -0600

    Test

--- src/com/weel/Main.java
+++ src/com/weel/Main.java
@@ -23,7 +23,7 @@ public class Main {
         PokedexWorker worker = new PokedexWorker();
         Pokedex pokedex = worker.convertInput(name, hp, type, moves);
 
-        System.out.println("This is our first test with git enabled");
+        System.out.println("This is our first test with git enabled!!");
 
     }
 }
