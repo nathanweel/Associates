b6d38d868df84286e56e5016576b691531a35b9c
Nathan Weel <nathanweel09@gmail.com>
2022-02-23 19:49:25 -0600

    Added functionality question

--- src/com/weel/Main.java
+++ src/com/weel/Main.java
@@ -9,32 +9,42 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        System.out.println("                                                \n" + " \\    / _  |  _  _  ._ _   _                     \n" + "  \\/\\/ (/_ | (_ (_) | | | (/_                    \n" + " |_)      ._   _. _|_ |_   _. ._        _   _  | \n" + " |_) \\/   | | (_|  |_ | | (_| | | \\/\\/ (/_ (/_ | \n" + "     /                                           ");
-
-
         //TODO: 5. All values coming from user **DONE**
         String name;
         int hp;
         String type;
         String moves;
 
-        //TODO: 6. Ask user for all inputs here **DONE**
-        System.out.println("What is the name of the Pokemon?");
-        name = System.console().readLine();
+        //Welcome Signature
+        System.out.println("                                                \n" + " \\    / _  |  _  _  ._ _   _                     \n" + "  \\/\\/ (/_ | (_ (_) | | | (/_                    \n" + " |_)      ._   _. _|_ |_   _. ._        _   _  | \n" + " |_) \\/   | | (_|  |_ | | (_| | | \\/\\/ (/_ (/_ | \n" + "     /                                           ");
 
-        System.out.println("How much health does this Pokemon have?");
-        hp = Integer.parseInt(System.console().readLine());
+        System.out.println("Would you like to add a new Pokemon or search for a Pokemon?\nType add or search");
+        String reply = System.console().readLine();
 
-        System.out.println("What type(s) is your Pokemon?");
-        type = System.console().readLine();
+        if (reply.equalsIgnoreCase("add")) {
+            //TODO: 6. Ask user for all inputs here **DONE**
+            System.out.println("What is the name of the Pokemon?");
+            name = System.console().readLine();
 
-        System.out.println("What move(s) can this Pokemon use?\nSeparate by coma");
-        moves = System.console().readLine();
+            System.out.println("How much health does this Pokemon have?");
+            hp = Integer.parseInt(System.console().readLine());
 
-        PokedexWorker worker = new PokedexWorker();
-        Pokedex pokedex = worker.convertInput(name, hp, type, moves);
+            System.out.println("What type(s) is your Pokemon?");
+            type = System.console().readLine();
 
-        worker.write(pokedex);
+            System.out.println("What move(s) can this Pokemon use?\nSeparate by coma");
+            moves = System.console().readLine();
 
+            PokedexWorker worker = new PokedexWorker();
+            Pokedex pokedex = worker.convertInput(name, hp, type, moves);
+
+            System.out.println("Writing credentials to a file...\n...\n...\n...DONE");
+            worker.write(pokedex);
+
+        } else if (reply.equalsIgnoreCase("search")) {
+            System.out.println("Functionality not yet added");
+        }else{
+            System.out.println("Unidentified entry");
+        }
     }
 }
